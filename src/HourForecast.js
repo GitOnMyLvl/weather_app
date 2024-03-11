25a18eeb4488d3a343d8481f443dfd07898cef87
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-03-11 17:52:15 +0100

    adds more css

--- src/HourForecast.js
+++ src/HourForecast.js
@@ -1,3 +1,5 @@
+import { parseISO, format } from 'date-fns';
+
 class HourForecast {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
@@ -10,6 +12,7 @@ class HourForecast {
       this.hourForecast.id = 'hourForecast';
       this.hourForecast.classList.add('hourForecast');
       this.container.appendChild(this.hourForecast);
+      this.setupScrolling();
     } else {
       this.hourForecast.innerHTML = '';
     }
@@ -18,13 +21,47 @@ class HourForecast {
 
     data.forecastHours.forEach((hour) => {
       const hourDiv = document.createElement('div');
+      const date = parseISO(hour.time);
+      const time = format(date, 'HH:mm');
       hourDiv.classList.add('hourDiv');
       hourDiv.innerHTML = `
-        <h3>${hour.time}</h3>
-        <p>Temperature: ${hour.temp}°C</p>
+        <p>${time}</p>
         <img src="${hour.icon}" alt="${hour.condition}">
+        <p>${hour.temp}°</p>
       `;
       this.hourForecast.appendChild(hourDiv);
+    });
+  }
+
+  setupScrolling() {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    this.hourForecast.addEventListener('mousedown', (e) => {
+      isDown = true;
+      this.hourForecast.classList.add('active');
+      startX = e.pageX - this.hourForecast.offsetLeft;
+      scrollLeft = this.hourForecast.scrollLeft;
+      e.preventDefault();
+    });
+
+    this.hourForecast.addEventListener('mouseleave', () => {
+      isDown = false;
+      this.hourForecast.classList.remove('active');
+    });
+
+    this.hourForecast.addEventListener('mouseup', () => {
+      isDown = false;
+      this.hourForecast.classList.remove('active');
+    });
+
+    this.hourForecast.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - this.hourForecast.offsetLeft;
+      const walk = (x - startX) * 3;
+      this.hourForecast.scrollLeft = scrollLeft - walk;
     });
   }
 }
